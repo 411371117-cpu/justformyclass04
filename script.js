b38606b002513338decba26909280ca0a3ecfd59
411371117-cpu <411371117@mail.nknu.edu.tw>
2026-06-05 12:37:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showTab(tabId){
+
+    let tabs =
+    document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab => {
+
+        tab.classList.remove("active");
+
+    });
+
+    document
+        .getElementById(tabId)
+        .classList.add("active");
+}
